ef75162b3267ac1bf18e811c3b3739224d0aa02f
cnt <150760001@qq.cm>
2017-01-28 00:52:16 +0800

    第三活动

--- app/src/main/java/com/example/cnt/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/cnt/activitytest/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.example.cnt.activitytest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -40,9 +43,31 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "You clicked Button 1",
-                        Toast.LENGTH_SHORT).show();
+                //Toast.makeText(FirstActivity.this, "You clicked Button 1",
+                //        Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                //Intent intent = new Intent("com.example.cnt.activitytest.ACTION_START");
+                //intent.addCategory("com.example.cnt.activitytest.MY_CATEGORY");
+                //Intent intent = new Intent(Intent.ACTION_DIAL);
+                //intent.setData(Uri.parse("tel:10086"));
+                //String data = "Hello SecondActivity";
+                //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                //intent.putExtra("extra_data", data);
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+                //startActivity(intent);
             }
         });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
